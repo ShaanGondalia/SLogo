255cde64326eef3d6af857ecce44d83fd3e0278e
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-13 17:06:48 -0500

    Added parser interface

---
+++ doc/plan/api/model/ParserInterface.java
@@ -0,0 +1,15 @@
+package slogo;
+
+/**
+ * This is the external API for the parser in the model. It provides an interface for parsing commands and returning a tree of commands.
+ *
+ */
+public interface ParserInterface{
+
+  /**
+   * Builds a tree of command objects based on the input from the controller.
+   * @param input the text input of the commands that the user entered in the view
+   * @return A tree of commands that represents the entire chain of commands the user input.
+   */
+  public Tree<Command> buildCommandTree(String input) throws Exception;
+}
