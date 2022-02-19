41e82569f488ef0fa2f8982a3905ad33ad6a3f12
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-19 18:32:42 -0500

    Improved documentation

--- src/main/java/slogo/model/Compiler.java
+++ src/main/java/slogo/model/Compiler.java
@@ -16,6 +16,11 @@ import slogo.model.exception.MissingArgumentException;
 import slogo.model.parser.Parser;
 import slogo.model.turtle.Turtle;
 
+/**
+ * Compiles user input into a queue of commands that can be executed. Depends on Turtle, Parser, and Command.
+ *
+ * @author Jake Heller and Shaan Gondalia
+ */
 public class Compiler {
 
   public static final String WHITESPACE = "\\s+";
@@ -23,28 +28,36 @@ public class Compiler {
   private static final String REFLECTION_RESOURCES = "model.Reflection";
   private final static String ERROR_MESSAGE = "Property %s is not a %s: %s";
 
-  private Turtle myTurtle;
   private Parser myParser;
   private Map<String, Double> myVariables;
   private final ResourceBundle parameterResources = ResourceBundle.getBundle(PARAMETER_RESOURCES);
   private final ResourceBundle reflectionResources = ResourceBundle.getBundle(REFLECTION_RESOURCES);
 
-  public Compiler () {
+  /**
+   * Creates an instance of a compiler for the given language.
+   */
+  public Compiler (String language) {
     myParser = new Parser();
-    myParser.addPatterns("English");
+    myParser.addPatterns(language);
     myParser.addPatterns("Syntax");
     myVariables = new HashMap<>();
-    myTurtle = new Turtle();
   }
 
-  public Queue<Command> compile(String program) throws MissingArgumentException {
+  /**
+   * Compiles a program into a queue of executable commands.
+   *
+   * @param program the string input of the program
+   * @param turtle the turtle to attach commands to
+   * @return a queue of executable commands
+   * @throws Exception if there is an issue compiling the program
+   */
+  public Queue<Command> compile(String program, Turtle turtle) throws Exception {
     //Scanner scanner = new Scanner(input);
     Queue<Command> commandQueue = new LinkedList<>();
 
     Stack<String> pendingCommands = new Stack<>();
     Stack<Double> values = new Stack<>();
 
-    // Split the input program by whitespace
     for (String token : program.split(WHITESPACE)) {
       String symbol = myParser.getSymbol(token);
       if (parameterResources.containsKey(symbol)){
@@ -64,7 +77,7 @@ public class Compiler {
           args.add(values.pop());
         }
         // Use reflection to create command
-        Command command = getCommand(pendingCommand, args);
+        Command command = getCommand(pendingCommand, turtle, args);
         commandQueue.add(command);
         pendingCommands.pop();
         values.add(command.returnValue());
@@ -82,14 +95,14 @@ public class Compiler {
   }
 
   // Returns an instance of a command using reflection
-  private Command getCommand(String symbol, List<Double> args){
+  private Command getCommand(String symbol, Turtle turtle, List<Double> args){
     String command = reflectionResources.getString(symbol).trim();
     try {
       // convert string into Java object that represents that Java class
       Class<?> clazz = Class.forName(command);
       // use reflection to find the appropriate constructor of that class to call to create a new instance
       Constructor<?> ctor = clazz.getDeclaredConstructor(Turtle.class, List.class);
-      return (Command)ctor.newInstance(myTurtle, args);
+      return (Command)ctor.newInstance(turtle, args);
     }
     catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException |
         InstantiationException | IllegalAccessException e) {
--- src/main/java/slogo/model/ModelTester.java
+++ src/main/java/slogo/model/ModelTester.java
@@ -1,16 +1,15 @@
 package slogo.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import slogo.model.command.Command;
-import slogo.model.command.turtle.Forward;
-import slogo.model.exception.MissingArgumentException;
+
+import slogo.model.turtle.Turtle;
 
 public class ModelTester {
 
-  public static void main(String[] args) throws MissingArgumentException {
-    Compiler c = new Compiler();
-    Queue<Command> q = c.compile("fd 50 + fd 50 50");
+  public static void main(String[] args) throws Exception {
+    Compiler c = new Compiler("English");
+    Turtle turtle = new Turtle();
+    Queue<Command> q = c.compile("fd 50 + fd 50 50", turtle);
   }
 }
