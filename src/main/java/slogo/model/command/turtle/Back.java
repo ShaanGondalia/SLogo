9ca4b59ef0c6e3a0de0380534cedf37fbc70c470
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-19 21:48:14 -0500

    Added back command

---
+++ src/main/java/slogo/model/command/turtle/Back.java
@@ -0,0 +1,45 @@
+package slogo.model.command.turtle;
+
+import java.util.List;
+import slogo.model.turtle.Turtle;
+import slogo.model.exception.MissingArgumentException;
+
+/**
+ * Class that represents a back (bk) command. Depends on TurtleCommand and Turtle.
+ *
+ * @author Shaan Gondalia
+ */
+public class Back extends TurtleCommand {
+
+  private final double pixels;
+  private static final int NUM_ARGS = 1;
+  /**
+   * Creates a back command
+   * @param args the arguments for the command (single argument for BK)
+   * @param turtle the Turtle that will be moved when the command is executed
+   * @throws MissingArgumentException if the list of arguments does not contain enough arguments
+   */
+  public Back(Turtle turtle, List<Double> args) throws MissingArgumentException {
+    super(turtle, args, NUM_ARGS);
+    pixels = args.get(0);
+  }
+
+  /**
+   * Moves the attached turtle back the given number of pixels
+   * @return the number of pixels the turtle will move
+   */
+  @Override
+  public Double execute() {
+    getTurtle().move(-pixels);
+    return pixels;
+  }
+
+  /**
+   * Returns the number of pixels the turtle will move
+   * @return the number of pixels the turtle will move
+   */
+  @Override
+  public Double returnValue() {
+    return pixels;
+  }
+}
