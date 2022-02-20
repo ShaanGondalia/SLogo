281899724d52f12d6bc2ae42446e9a2eda559253
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-20 16:32:38 -0500

    Renamed backward command

--- src/main/java/slogo/model/command/turtle/Backward.java
+++ src/main/java/slogo/model/command/turtle/Backward.java
@@ -5,27 +5,27 @@ import slogo.model.turtle.Turtle;
 import slogo.model.exception.MissingArgumentException;
 
 /**
- * Class that represents a back (bk) command. Depends on TurtleCommand and Turtle.
+ * Class that represents a backward (bk) command. Depends on TurtleCommand and Turtle.
  *
  * @author Shaan Gondalia
  */
-public class Back extends TurtleCommand {
+public class Backward extends TurtleCommand {
 
   private final double pixels;
   private static final int NUM_ARGS = 1;
   /**
-   * Creates a back command
+   * Creates a backward command
    * @param args the arguments for the command (single argument for BK)
    * @param turtle the Turtle that will be moved when the command is executed
    * @throws MissingArgumentException if the list of arguments does not contain enough arguments
    */
-  public Back(Turtle turtle, List<Double> args) throws MissingArgumentException {
+  public Backward(Turtle turtle, List<Double> args) throws MissingArgumentException {
     super(turtle, args, NUM_ARGS);
     pixels = args.get(0);
   }
 
   /**
-   * Moves the attached turtle back the given number of pixels
+   * Moves the attached turtle backward the given number of pixels
    * @return the number of pixels the turtle will move
    */
   @Override
--- src/test/java/slogo/model/command/turtle/BackwardTest.java
+++ src/test/java/slogo/model/command/turtle/BackwardTest.java
@@ -6,16 +6,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import slogo.model.command.math.Sum;
 import slogo.model.exception.MissingArgumentException;
 import slogo.model.turtle.Turtle;
 
 /**
- * Tests for Back Command
+ * Tests for Backward Command
  *
  * @author Shaan Gondalia
  */
-public class BackTest {
+public class BackwardTest {
 
   private static final double ARG_1 = 50;
   private static final double ARG_2 = 100;
@@ -38,7 +37,7 @@ public class BackTest {
     List<Double> args = new ArrayList<>();
     args.add(ARG_1);
     args.add(ARG_2);
-    Back c = new Back(myTurtle, args);
+    Backward c = new Backward(myTurtle, args);
     assertEquals(ARG_1, c.returnValue());
     assertEquals(ARG_1, c.execute());
   }
@@ -47,7 +46,7 @@ public class BackTest {
   void testCorrectArgs() throws MissingArgumentException {
     List<Double> args = new ArrayList<>();
     args.add(ARG_1);
-    Back c = new Back(myTurtle, args);
+    Backward c = new Backward(myTurtle, args);
     assertEquals(c.returnValue(), ARG_1);
     double xBefore = myTurtle.getPose().x();
     double yBefore = myTurtle.getPose().y();
