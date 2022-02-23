303903bee0394b83546e89ddc6aee7d03aabc15d
Andy He <andy.he@duke.edu>
2022-02-22 22:04:12 -0500

    Language Probe Selects a Language - Currently only works if english is selected

--- src/main/java/slogo/view/LanguageProbe.java
+++ src/main/java/slogo/view/LanguageProbe.java
@@ -1,7 +1,13 @@
 package slogo.view;
 
 import java.awt.Dimension;
+import java.util.ResourceBundle;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import slogo.Errors;
 
 /**
  * Used to probe what language the whole program will be in
@@ -9,16 +15,54 @@ import javafx.stage.Stage;
  * @author Andy S He
  */
 public class LanguageProbe {
-  private static final String TITLE = "Language Probe";
-  private static final Dimension SIZE = new Dimension(400,400);
 
-  public LanguageProbe(){
-    Stage stage = new Stage();
+  private static final String TITLE = "Language Probe";
+  private static final Dimension SIZE = new Dimension(400, 400);
+  private static final String RESOURCE_LANGUAGES = "slogo.languages.LangaugeOptions";
+
+  private final ResourceBundle myLanguages;
+  private final Stage myStage;
+  private String language;
+
+  public LanguageProbe() {
+    myLanguages = ResourceBundle.getBundle(RESOURCE_LANGUAGES);
+
+    myStage = new Stage();
+
+    Scene scene = new Scene(makeOptions(), SIZE.width, SIZE.height);
+    myStage.setTitle(TITLE);
+    myStage.setScene(scene);
+    try {
+      myStage.showAndWait();
+    }
+    catch(Exception e){
+      Errors.showAndClose(e.getMessage());
+    }
+
+  }
+
+  private TilePane makeOptions() {
+    TilePane root = new TilePane();
+    for (String key : myLanguages.keySet()) {
+      makeOption(root, key);
+    }
+    root.setAlignment(Pos.CENTER);
+    return root;
+  }
+
+  private void makeOption(TilePane root, String key) {
+    Button b = new Button();
+    b.setText(myLanguages.getString(key));
+    b.setOnAction(e -> {
+      language = key;
+      myStage.close();
+    });
+    root.getChildren().add(b);
   }
 
   @Override
-  public String toString(){
-    return "English";
+  public String toString() {
+    return language;
   }
 
 }
