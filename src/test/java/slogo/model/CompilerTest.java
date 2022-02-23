a512c0451c732df8b0b456e7d209367008ef1d6d
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-22 20:07:27 -0500

    Refactored all remaining model tests

--- src/test/java/slogo/model/CompilerTest.java
+++ src/test/java/slogo/model/CompilerTest.java
@@ -5,8 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import slogo.model.command.Command;
 import slogo.model.exception.MissingArgumentException;
 import slogo.model.exception.SymbolNotFoundException;
 import slogo.model.turtle.Turtle;
@@ -38,43 +40,51 @@ public class CompilerTest {
 
   @Test
   void testIncorrect() {
-    assertThrows(MissingArgumentException.class, () -> compiler.run(INCORRECT_PROGRAM, myTurtles));
+    assertThrows(MissingArgumentException.class,
+        () -> compiler.compile(INCORRECT_PROGRAM, myTurtles));
   }
 
   @Test
   void testBasic() throws Exception {
-    compiler.run(BASIC_PROGRAM, myTurtles);
+    run(compiler.compile(BASIC_PROGRAM, myTurtles));
     assertEquals(50, myTurtles.get(0).getPose().y());
   }
 
   @Test
   void testTwoArg() throws Exception {
-    compiler.run(TWO_ARG_PROGRAM, myTurtles);
+    run(compiler.compile(TWO_ARG_PROGRAM, myTurtles));
     assertEquals(100, myTurtles.get(0).getPose().y());
   }
 
   @Test
   void testMulti() throws Exception {
-    compiler.run(MULTI_PROGRAM, myTurtles);
+    run(compiler.compile(MULTI_PROGRAM, myTurtles));
     assertEquals(150, myTurtles.get(0).getPose().y());
   }
 
   @Test
   void testComplex() throws Exception {
-    compiler.run(COMPLEX_PROGRAM, myTurtles);
+    run(compiler.compile(COMPLEX_PROGRAM, myTurtles));
     assertEquals(150, myTurtles.get(0).getPose().y());
     assertEquals(270, myTurtles.get(0).getPose().bearing());
   }
 
   @Test
   void testUnknown() throws Exception {
-    assertThrows(SymbolNotFoundException.class, () -> compiler.run(UNKNOWN_PROGRAM, myTurtles));
+    assertThrows(SymbolNotFoundException.class, () -> compiler.compile(UNKNOWN_PROGRAM, myTurtles));
   }
 
   @Test
   void testParameterOrder() throws Exception {
     String program = "sum 50 fd";
-    assertThrows(MissingArgumentException.class, () -> compiler.run(program, myTurtles));
+    assertThrows(MissingArgumentException.class, () -> compiler.compile(program, myTurtles));
+  }
+
+  private void run(Queue<Command> q) throws MissingArgumentException {
+    while (!q.isEmpty()) {
+      Command command = q.remove();
+      command.execute();
+    }
   }
 
 }
