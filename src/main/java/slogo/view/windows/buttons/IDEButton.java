dc7ae3a3b38ef20be3e850210b56138ad0a6379b
Andy He <andy.he@duke.edu>
2022-02-26 16:48:24 -0500

    java doc ->  IDEButton

--- src/main/java/slogo/view/windows/buttons/IDEButton.java
+++ src/main/java/slogo/view/windows/buttons/IDEButton.java
@@ -3,5 +3,10 @@ package slogo.view.windows.buttons;
 import slogo.view.util.ButtonUtil;
 
 public interface IDEButton {
+
+  /**
+   * Does that action that the button should do
+   * @param info contains the information that each of the buttons need to function
+   */
   void doAction(ButtonUtil info);
 }
