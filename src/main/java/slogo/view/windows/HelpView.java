d6b5ea90c43710881a42604ba4299eb798d7987c
Andy He <andy.he@duke.edu>
2022-02-27 15:26:09 -0500

    finished help view v1

--- src/main/java/slogo/view/windows/HelpView.java
+++ src/main/java/slogo/view/windows/HelpView.java
@@ -4,28 +4,36 @@ import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import slogo.Errors;
 
-public class HelpView extends Display{
+public class HelpView extends Display {
 
   private static final String TITLE = "Help View";
-  private static final Dimension DIM = new Dimension(400,400);
+  private static final Dimension DIM = new Dimension(400, 400);
   private static final String FILE_PATH = "src/main/resources/view/reference";
 
   private String myContent;
   private Text myContentTextBox;
-  public HelpView(){
+  private VBox myButtons;
+  private File[] myFiles;
+
+  public HelpView() {
+
+    File path = new File(FILE_PATH);
+    myFiles = path.listFiles();
+
     myContent = "";
     BorderPane borderPane = new BorderPane();
 
     ScrollPane options = new ScrollPane();
-    VBox buttons = new VBox();
-    options.setContent(buttons);
+    myButtons = new VBox();
+    options.setContent(myButtons);
 
     ScrollPane description = new ScrollPane();
     myContentTextBox = new Text(myContent);
@@ -39,19 +47,33 @@ public class HelpView extends Display{
     stage.show();
   }
 
+  private void makeHelperButtons() {
+    for (File file : myFiles) {
+      Button b = new Button();
+      b.setText(file.getName());
+      b.setOnAction((e) -> {
+        try {
+          setDescContent(file.getName());
+        } catch (FileNotFoundException ex) {
+          Errors.showError("internal error");
+        }
+      });
+      myButtons.getChildren().add(b);
+    }
+  }
+
   private void setDescContent(String name) throws FileNotFoundException {
     myContent = "";
-    File path = new File(FILE_PATH);
-    File[] files = path.listFiles();
-    for (int i = 0; i < files.length; i++){
-      if (files[i].isFile() && files[i].getName().equals(name)){ //this line weeds out other directories/folders
-        Scanner scanner = new Scanner(files[i]);
-        while (scanner.hasNextLine()){
+
+    for (int i = 0; i < myFiles.length; i++) {
+      if (myFiles[i].isFile() && myFiles[i].getName()
+          .equals(name)) { //this line weeds out other directories/folders
+        Scanner scanner = new Scanner(myFiles[i]);
+        while (scanner.hasNextLine()) {
           myContent += scanner.nextLine() + "\n";
         }
       }
     }
     myContentTextBox.setText(myContent);
   }
-
 }
