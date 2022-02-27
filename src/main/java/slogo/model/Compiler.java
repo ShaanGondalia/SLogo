d02a5594e643e346b9d78f5034e222b99db96fc2
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-27 17:23:10 -0500

    Slightly refactored compiler - more to come

--- src/main/java/slogo/model/Compiler.java
+++ src/main/java/slogo/model/Compiler.java
@@ -32,6 +32,16 @@ public class Compiler {
   private CommandFactory commandFactory;
   private final ResourceBundle exceptionResources;
 
+  private Stack<String> pendingCommandsInContext = new Stack<>();
+  private Stack<Value> valuesInContext = new Stack<>();
+  private Stack<Deque<Command>> listsInContext = new Stack<>();
+
+  private Stack<Integer> valuesBeforeInContext = new Stack<>();
+  private Stack<Integer> listsBeforeInContext = new Stack<>();
+
+  private Stack<Deque<Command>> commandQueuesInContext = new Stack<>();
+
+
   /**
    * Creates an instance of a compiler for the given language.
    */
@@ -58,74 +68,78 @@ public class Compiler {
     // will be changed when we can have multiple turtles
     Turtle turtle = turtles.get(0);
 
-    Stack<String> pendingCommands = new Stack<>();
-    Stack<Value> values = new Stack<>();
-    Stack<Deque<Command>> lists = new Stack<>();
-    Stack<Integer> valuesBefore = new Stack<>();
-    Stack<Integer> listsBefore = new Stack<>();
-    Stack<Deque<Command>> queueStack = new Stack<>();
-
     for (String token : program.split(WHITESPACE)) {
-      String symbol = myParser.getSymbol(token);
 
-      if (commandFactory.isCommand(symbol)) {
-        pendingCommands.push(symbol);
-        valuesBefore.push(values.size());
-        listsBefore.push(lists.size());
-      } else if (symbol.equals("Constant")) {
-        values.push(new Value(Double.parseDouble(token)));
-      } else if (symbol.equals("Variable")) {
-        if (!myVariables.containsKey(token)) {
-          myVariables.put(token, new Value());
-        }
-        values.push(myVariables.get(token));
-      } else if (symbol.equals("UserCommand")) {
-        if (pendingCommands.peek().equals("MakeUserInstruction")) {
-          // TODO: figure out how make user instruction works
-          int inputs = 0; // need to figure out how many inputs user instruction takes
-          commandFactory.makeCommand(token, inputs);
-        } else {
-          throw new SymbolNotFoundException(
-              String.format(exceptionResources.getString("SymbolNotFound"), symbol));
-        }
-      } else if (symbol.equals("ListStart")) {
-        queueStack.push(new LinkedList<>());
-      } else if (symbol.equals("ListEnd")) {
-        lists.push(queueStack.pop());
-        values.pop(); // This is needed to remove the value from the final command in the body of a list
-      }
-
+      handleToken(token);
       // LOGIC:
       // After a command is added, we can only resolve the commands arguments once numInputs values have been added to the value stack.
       // We must keep track of how many values have been added to the stack after each command has been added.
       // We know the number of inputs each command requires, and we know the size of the values stack when each command is added.
       // We can create another data structure that tracks this information, and use it to determine when
 
-      while (!pendingCommands.isEmpty()
-          && commandFactory.getNumInputs(pendingCommands.peek())
-          <= values.size() - valuesBefore.peek()
-          && commandFactory.getNumListInputs(pendingCommands.peek())
-          <= lists.size() - listsBefore.peek()) {
-        String pendingCommand = pendingCommands.pop();
-        valuesBefore.pop();
-        listsBefore.pop();
-        Command command = commandFactory.getCommand(pendingCommand, turtle, values, lists);
-        values.add(command.returnValue());
-        if (!queueStack.empty()) {
-          queueStack.peek().addLast(command);
+      while (!pendingCommandsInContext.isEmpty()
+          && commandFactory.getNumInputs(pendingCommandsInContext.peek())
+          <= valuesInContext.size() - valuesBeforeInContext.peek()
+          && commandFactory.getNumListInputs(pendingCommandsInContext.peek())
+          <= listsInContext.size() - listsBeforeInContext.peek()) {
+        String pendingCommand = pendingCommandsInContext.pop();
+        valuesBeforeInContext.pop();
+        listsBeforeInContext.pop();
+        Command command = commandFactory.getCommand(pendingCommand, turtle, valuesInContext, listsInContext);
+        valuesInContext.add(command.returnValue());
+        //System.out.println(pendingCommand);
+        //values.forEach((value) -> System.out.println(value));
+        if (!commandQueuesInContext.empty()) {
+          commandQueuesInContext.peek().addLast(command);
         } else {
           commandQueue.addLast(command);
         }
-        if (pendingCommands.isEmpty()) {
-          values.clear();
+        if (pendingCommandsInContext.isEmpty()) {
+          valuesInContext.clear();
         }
       }
     }
-    if (!pendingCommands.empty()) {
+    if (!pendingCommandsInContext.empty()) {
       throw new MissingArgumentException(
-          String.format(exceptionResources.getString("MissingArgument"), pendingCommands.peek()));
+          String.format(exceptionResources.getString("MissingArgument"), pendingCommandsInContext.peek()));
     }
     return commandQueue;
   }
+
+
+  private void handleToken(String token) throws SymbolNotFoundException {
+    String symbol = myParser.getSymbol(token);
+
+    if (commandFactory.isCommand(symbol)) {
+      pendingCommandsInContext.push(symbol);
+      valuesBeforeInContext.push(valuesInContext.size());
+      listsBeforeInContext.push(listsInContext.size());
+    } else if (symbol.equals("Constant")) {
+      valuesInContext.push(new Value(Double.parseDouble(token)));
+    } else if (symbol.equals("Variable")) {
+      if (!myVariables.containsKey(token)) {
+        myVariables.put(token, new Value());
+      }
+      valuesInContext.push(myVariables.get(token));
+    } else if (symbol.equals("UserCommand")) {
+      if (pendingCommandsInContext.peek().equals("MakeUserInstruction")) {
+        // TODO: figure out how make user instruction works
+        int inputs = 0; // need to figure out how many inputs user instruction takes
+        commandFactory.makeCommand(token, inputs);
+      } else {
+        throw new SymbolNotFoundException(
+            String.format(exceptionResources.getString("SymbolNotFound"), symbol));
+      }
+    } else if (symbol.equals("ListStart")) {
+      commandQueuesInContext.push(new LinkedList<>());
+    } else if (symbol.equals("ListEnd")) {
+      Deque<Command> commandList = commandQueuesInContext.pop();
+      if(!commandList.isEmpty()) {
+        listsInContext.push(commandList);
+      }
+    }
+  }
+
+
 
 }
