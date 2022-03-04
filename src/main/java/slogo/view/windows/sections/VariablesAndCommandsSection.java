b8a65f0f3c7fd1ee56956a8fa49b882015c09491
Andy He <andy.he@duke.edu>
2022-03-03 20:36:37 -0500

    added ability to click to change variables

--- src/main/java/slogo/view/windows/sections/VariablesAndCommandsSection.java
+++ src/main/java/slogo/view/windows/sections/VariablesAndCommandsSection.java
@@ -1,12 +1,16 @@
 package slogo.view.windows.sections;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import slogo.Errors;
 import slogo.controller.Controller;
 import slogo.model.compiler.Parser;
 import slogo.view.util.Runner;
@@ -27,12 +31,16 @@ public class VariablesAndCommandsSection implements IDESection {
   private static final String COM_SP_ID = "com_pane";
   private static final String COM_TF_ID = "com_text";
   private static final String COM_STARTING_TEXT = "Commands";
+  private static final String DIA_TITLE = "title";
+  private static final String DIA_DESC = "desc";
+  private static final String DIA_RESOURCES_PATH = "view.variableDialog.";
 
   private static final int VAR_WIDTH = 100;
   private static final int COM_WIDTH = 200;
 
   private ScrollPane variableScrollPane;
   private Text variableTextField;
+  private VBox variableVBox;
 
   private ScrollPane commandScrollPane;
   private Text commandTextField;
@@ -65,12 +73,16 @@ public class VariablesAndCommandsSection implements IDESection {
   }
 
   private void setVariableSide() {
+
     variableTextField = new Text(myResources.getString(VAR_STARTING_TEXT));
     variableTextField.setId(VAR_TF_ID);
-
+    variableVBox = new VBox();
+    BorderPane variableBorderPane = new BorderPane();
+    variableBorderPane.setTop(variableTextField);
+    variableBorderPane.setCenter(variableVBox);
     variableScrollPane = new ScrollPane();
     variableScrollPane.setId(VAR_SP_ID);
-    variableScrollPane.setContent(variableTextField);
+    variableScrollPane.setContent(variableBorderPane);
     variableScrollPane.setPrefViewportWidth(VAR_WIDTH);
   }
 
@@ -84,12 +96,31 @@ public class VariablesAndCommandsSection implements IDESection {
     commandScrollPane.setPrefViewportWidth(COM_WIDTH);
   }
 
-  private void makeVariableButton(String variableName, String value){
+  private void makeVariableButton(String variableName, String value) {
     Button b = new Button(formatVariableString(variableName, value));
     b.setMinWidth(VAR_WIDTH);
-    b.setOnAction((e) -> {
+    b.setOnAction((e) -> openAndSaveNewVariableDialog(variableName, value));
+    variableVBox.getChildren().add(b);
+  }
 
-    });
+  private void openAndSaveNewVariableDialog(String variableName, String value) {
+    ResourceBundle dialogResources = ResourceBundle.getBundle(DIA_RESOURCES_PATH + myLanguage);
+    TextInputDialog dialog = new TextInputDialog(value);
+    dialog.setTitle(dialogResources.getString(DIA_TITLE));
+    dialog.setHeaderText(dialogResources.getString(DIA_TITLE));
+    dialog.setContentText(dialogResources.getString(DIA_DESC));
+    Optional<String> result = dialog.showAndWait();
+    String content = dialog.getEditor().getText();
+
+    double toSet = 0;
+    try {
+      toSet = Double.parseDouble(content);
+    } catch (Exception ex) {
+      Errors.showError(ex.getMessage());
+    }
+    if (result.isPresent()) {
+      myRunner.runQuietly(makeVariableSetCommand(variableName, String.valueOf(toSet)));
+    }
   }
 
   /**
@@ -101,20 +132,14 @@ public class VariablesAndCommandsSection implements IDESection {
   }
 
   private void updateVariables() {
+    variableVBox.getChildren().clear();
     Map<String, String> varList = myController.getMapData("variables");
-    String toDisplay = myResources.getString(VAR_STARTING_TEXT) + NEW_LINE;
     for (String variableName : varList.keySet()) {
-      String variableValue = varList.get(variableName);
-      toDisplay += variableName.substring(1);
-      toDisplay += DELIMITER;
-      toDisplay += variableValue;
-      toDisplay += NEW_LINE;
+      makeVariableButton(variableName, varList.get(variableName));
     }
-
-    variableTextField.setText(toDisplay);
   }
 
-  private String formatVariableString(String name, String value){
+  private String formatVariableString(String name, String value) {
     String s = name;
     s = s.substring(1);
     s += DELIMITER;
