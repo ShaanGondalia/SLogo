dd997e25f3790abd3ae97ba7fa145f11e78b9f24
Andy He <andy.he@duke.edu>
2022-03-04 16:22:24 -0500

    Testing - TurtleNumSplashTest

---
+++ src/test/java/slogo/view/windows/TurtleNumSplashTest.java
@@ -0,0 +1,28 @@
+package slogo.view.windows;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+class TurtleNumSplashTest extends DukeApplicationTest {
+  private Splash ls;
+  private Button two;
+
+  @Override
+  public void start(Stage stage) {
+    ls = new TurtleNumSplash("English");
+    stage = new Stage();
+    stage.setScene(ls.getMyScene());
+    stage.show();
+    two = lookup("#2").query();
+  }
+
+  @Test
+  void turtleNumToUse(){
+    clickOn(two);
+    assertEquals(ls.toString(), String.valueOf(2));
+  }
+}
