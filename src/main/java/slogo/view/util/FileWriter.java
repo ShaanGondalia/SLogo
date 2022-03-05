a91b609ee1fdd47ff717a188b422ab9677f384c3
Andy He <andy.he@duke.edu>
2022-03-04 20:16:27 -0500

    java-docs -> FileWriter

--- src/main/java/slogo/view/util/FileWriter.java
+++ src/main/java/slogo/view/util/FileWriter.java
@@ -6,8 +6,20 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import slogo.Errors;
 
+/**
+ * A class that can write text to a file
+ *
+ * @author Andy S. He
+ * @see slogo.view.windows.buttons.VariablesAndCommandsOut
+ * @see slogo.view.windows.buttons.CommandHistoryOut
+ */
 public class FileWriter {
   private static final String TITLE = "Save";
+
+  /**
+   * Default constructor
+   * @param toWrite the text that will be written to the file
+   */
   public FileWriter(String toWrite){
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle(TITLE);
